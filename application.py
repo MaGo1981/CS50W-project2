3d32e913837e937a67d8474ff34e0f57916d5be9
MaGo1981 <top.underdog01@gmail.com>
2018-10-13 21:11:52 +0200

    Display name

--- application.py
+++ application.py
@@ -1,105 +1,17 @@
 import os
 
-from flask import Flask, render_template, jsonify, request, flash, redirect, session, url_for
-from flask_session import Session
-from models import *
-from functools import wraps
-
-from sqlalchemy import create_engine
-from sqlalchemy.orm import scoped_session, sessionmaker
-
-import requests
-
+from flask import Flask, render_template
+from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-db.init_app(app)
+# app = Flask(__name__, instance_relative_config=True)
+app.config.from_object('config')
+# app.config.from_pyfile('config.py')
 
-# Check for environment variable
-if not os.getenv("DATABASE_URL"):
-    raise RuntimeError("DATABASE_URL is not set")
-
-# Configure session to use filesystem
-app.config["SESSION_PERMANENT"] = False
-app.config["SESSION_TYPE"] = "filesystem"
-Session(app)
+# app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
+socketio = SocketIO(app)
 
 
-# Set up database
-# engine = create_engine(os.getenv("DATABASE_URL"))
-# db = scoped_session(sessionmaker(bind=engine))
-
-
-def login_required(test):
-    @wraps(test)
-    def wrap(*args, **kwargs):
-        if 'logged_in' in session:
-            return test(*args, **kwargs)
-        else:
-            flash('You need to login first.')
-            return redirect(url_for('login'))
-    return wrap
-
-
-
-@app.route("/")
+@app.route('/')
 def index():
-    return render_template("index.html")
-
-
-@app.route("/register", methods=["GET","POST"])
-def register():
-    name = request.form.get("name")
-    print(name)
-    email = request.form.get("email")
-    print(email)
-    passw = request.form.get("passw")
-    print(passw)
-    repeatPass = request.form.get("repeatPass")
-    if request.method == 'POST':
-        if passw == repeatPass:
-            new_user = User(name=name, email=email, passw=passw)
-            print(new_user)
-            db.session.add(new_user)
-            db.session.commit()
-            return redirect(url_for('login'))
-        else:
-            message = "Passwords do not match, please try again!"
-            return render_template("error.html", message=message)
-    return render_template("register.html")
-
-
-
-@app.route("/login", methods=["GET","POST"])
-def login():
-    session['logged_in']=False
-    session['user_id'] = None
-    session['name'] = None
-    if request.method == 'POST':
-        user = User.query.filter_by(name=request.form.get('username')).first() # returns all columns; for specific column use user.column!
-        print(user)
-        print(user.passw)
-        print(request.form.get('pass'))
-
-        if user is not None and user.passw == request.form.get('pass'):
-            session['logged_in'] = True #RuntimeError: The session is unavailable because no secret key was set. Set the secret_key on the application to something unique and secret.
-            session['user_id'] = user.id
-            session['name'] = user.name
-            return redirect(url_for('main'))
-        else:
-            message = "Wrong username or password, please try again!"
-            return render_template("error.html", message=message)
-    return render_template("login.html")
-
-@app.route("/logout", methods=["GET","POST"])
-def logout():
-    session.pop('logged_in', None)
-    session.pop('name', None)
-    session.pop('user_id', None)    
-    return render_template("login.html")
-
-
-@app.route("/main")
-def main():
-    return render_template("main.html")
+	return render_template('index.html') 
---
+++ static/index.js
@@ -0,0 +1,21 @@
+document.addEventListener('DOMContentLoaded', () => {
+    /*form tag selected - radi onsubmit?*/
+    const form = document.querySelector('form');
+    /*id displayname selected*/
+    const displayName = document.querySelector('#displayName');
+    /*id greeting selected*/
+    const greeting = document.querySelector('#greeting');
+
+    form.onsubmit = () => {
+        // Save chosen displayName 
+        localStorage.setItem('displayName', displayName.value);
+
+        // Add a greeting
+        // greeting.append('Hi there, ' + displayName.value + '!');
+        // document.querySelector('#greeting').innerHTML = 'Hello there, ' + displayName.value + '!';
+        document.querySelector('#greeting').innerHTML = 'Hello there, ' + localStorage.getItem('displayName') + '!';
+        
+        // Stop form from submitting to some other page, or reloading the same page
+        return false;
+    };
+});
