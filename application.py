26dca69f84506c911af6b333efb972b59e337a44
MaGo1981 <top.underdog01@gmail.com>
2018-10-21 11:17:43 +0200

    name status is working

--- application.py
+++ application.py
@@ -1,3 +1,5 @@
+# THIS PART OF THE CODE IS RUNNING ON THE SERVER
+
 import os
 
 from flask import Flask, render_template
@@ -12,6 +14,31 @@ app.config.from_object('config')
 socketio = SocketIO(app)
 
 
+# 'MaGo1981' temporarily taken for testing
+display_names = ['MaGo1981']
+
+
 @app.route('/')
 def index():
-	return render_template('index.html') 
+        return render_template('index.html') 
+
+
+@app.route('/names')
+def names():
+    return '{}'.format(display_names)
+
+
+# Check if username exists
+@socketio.on('serverName check') # 2. when the socket detects this event called 'serverName check' emited from index.js - line 41
+def name_check(data):
+    result = {}
+    name_req = str(data['name']) # first take the data, and get out the name, save it inside variable called name_req
+    result['name'] = name_req    # save the data into result (check out JSON for result)
+
+    if (name_req not in display_names):
+        result['result'] = True # index.js - line 54
+        display_names.append(name_req) # delete line 16?
+        emit('serverName result', result)
+    else:
+        result['result'] = False
+        emit('serverName result', result)
--- static/index.js
+++ static/index.js
@@ -1,24 +1,65 @@
+// THIS PART OF THE CODE IS RUNNING LOCALLY ON OUR PC
 
 // DOMContentLoaded (DOM = Document Object Model)- događaj kojeg pokreće završetak učitavanja stranice u web browser!
-// element.addEventListener(event, function, useCapture)
-document.addEventListener('DOMContentLoaded', () => {
-    /*form tag selected - radi onsubmit?*/
-    const form = document.querySelector('form');
-    /*id displayname selected*/
-    const displayName = document.querySelector('#displayName');
-    /*id greeting selected*/
-    const greeting = document.querySelector('#greeting');
+// element.addEventListener(event, function, useCapture) - secound event is a function (higher order functions)
+document.addEventListener('DOMContentLoaded', () => { 
+	/*id status selected*/
+	const currentStatus = document.querySelector('#status');
+	var nameForm = ``;
+	var nameField = ``;
+	var nameError = ``;
+	var currentName = localStorage.getItem('displayName');
 
-    form.onsubmit = () => {
-        // Save chosen displayName 
-        localStorage.setItem('displayName', displayName.value);
+	// Connect to websocket
+	var socket = io.connect(location.protocol + '//' + document.domain + ':'
+						    + location.port);
 
-        // Add a greeting
-        // greeting.append('Hi there, ' + displayName.value + '!');
-        // document.querySelector('#greeting').innerHTML = 'Hello there, ' + displayName.value + '!';
-        document.querySelector('#greeting').innerHTML = 'Hello there, ' + localStorage.getItem('displayName') + '!';
-        
-        // Stop form from submitting to some other page, or reloading the same page
-        return false;
-    };
+	// If no display name stored, ask to submit one
+	if (currentName) {
+		const statusContent = `Logged in as ${currentName}`; // templated literal - like formated strings in Python (backtick simbol)   
+		currentStatus.innerHTML = statusContent;
+	} else {
+		const statusContent = `
+			<form id="nameForm">
+			<input type="text" placeholder="Choose a display name" 
+			 id="nameField" autofocus autocomplete-"off" required>
+			<button type="submit">Submit</button>'
+			</form>
+			<div id="nameError"></div>
+		`;
+		currentStatus.innerHTML = statusContent;
+
+		nameForm = document.querySelector('#nameForm');
+		nameField = document.querySelector('#nameField');
+		nameError = document.querySelector('#nameError');
+	}
+	
+	// Display name submission 
+	nameForm.onsubmit = () => {
+		// Check if inputted name exists on server
+		const nameReq = nameField.value; /// nameReq is input form nameField. use getItem?
+		socket.emit('serverName check', {'name': nameReq}); // 1. WE EMIT EVENT 'serverName check' to the webserver, by passing in data assosiated with this event (JSON OBJECT)
+
+		// Clear submission box, stop form from submitting
+		nameField.value = '';
+		//form.reset();
+		return false;  // do not load new page
+	};
+
+	// Save displayName if check succeeded 
+	// Socket listen to particular events
+	socket.on('serverName result', data => { // arrow function that takes as input variable data that is emited by socket
+		/// data is input from the user?
+		// if result key is True - application.py line 39
+		if (data.result) {
+			localStorage.setItem('displayName', data.name);
+			currentName = data.name; /// use setItem? search JSON or data.
+
+			const statusContent = `Welcome ${currentName}`;
+			currentStatus.innerHTML = statusContent;
+		}
+		// if result key is False - application.py line 43 (testiras s MaGo1981 ili nekim od ostalih imena s rute /names)
+		else  
+			nameError.innerHTML = `Sorry, please try another name.`;
+	});
 });
